6edcaa73c0a1d1f9eb6231724a60e6d126eb1424
OIgaB <linkbolya@gmail.com>
2023-06-27 09:49:24 +0300

    Generate 3 actions for authorization

---
+++ src/redux/authSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { loginThunk, getProfileThunk } from './operations';
+
+const initialState = {   
+    access_token: '',
+    isLoading: false,
+    error: '',
+    profile: null,
+};
+
+const handlePending = (state) => {
+    state.isLoading = true;
+    state.error = '';
+}
+
+const handleFulfilled = (state, { payload }) => { 
+    state.isLoading = false;
+    state.access_token = payload.access_token;
+}
+
+const handleFulfilledProfile = (state, { payload }) => {  
+    state.isLoading = false;
+    state.items = payload;
+}
+
+const handleRejected = (state, { error, payload }) => {
+    state.isLoading = false;
+    state.error = payload ?? error.message;
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        logOut: (state) => {
+            state.access_token = ""
+            state.isLoading = false
+            state.error = ''
+            state.profile = null
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginThunk.fulfilled, handleFulfilled)
+            .addCase(getProfileThunk.fulfilled, handleFulfilledProfile)
+            //спільні ф-ції обробки стану pending/rejected:
+            .addMatcher(({ type }) => type.endsWith('/pending'), handlePending)
+            .addMatcher(({ type }) => type.endsWith('/rejected'), handleRejected);
+    }
+});
+
+export const authReducer = authSlice.reducer;
+export const { logOut } = authSlice.actions;
